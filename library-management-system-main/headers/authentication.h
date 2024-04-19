fc4e2efadf07eed7a4569b8d98e5f8f00d56e6eb
Abhinav-Sudini <149574715+Abhinav-Sudini@users.noreply.github.com>
2024-04-19 15:37:50 +0530

    Revert "Removed the Student structure"

--- library-management-system-main/headers/authentication.h
+++ library-management-system-main/headers/authentication.h
@@ -1,8 +1,13 @@
 #include "./data-types.h"
+#include "./miscellaneous.h"
+// #include "./librarySystem.h"
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+struct Student student;
+struct Student admin;
 
 int login=0;
 int ADMIN=0;
@@ -12,6 +17,7 @@ void registerCredentials(struct Credentials);
 struct Credentials fetchCredentials(char [50]);
 void registerUser();
 struct Student loginUser();
+
 
 void registerCredentials(struct Credentials credential)
 {
@@ -30,6 +36,7 @@ struct Credentials fetchCredentials(char email[50])
 {
     struct Credentials credential;
     FILE *file;
+    char t;
 
     char fileName[50];
     sprintf(fileName, "credentials/%s.bin", email);
@@ -38,9 +45,11 @@ struct Credentials fetchCredentials(char email[50])
 
     if(file==NULL)
     {
-        struct Credentials temp={"null","null"};
+        struct Credentials temp;
+        strcpy(temp.email,"null");
         return temp;
     }
+
 
     fread(&credential, sizeof(struct Credentials), 1, file);
     fclose(file);
@@ -72,19 +81,24 @@ struct Student fetchData(char email[50])
 
     fread(&student, sizeof(struct Student), 1, file);
     fclose(file);
+    
     return student;
 }
 
 void registerUser()
 {
     char name[50],roll[50],pass[50];
-    printf("Enter your name: ");
+    printf("Enter your first name: ");
     scanf("%s",name);
     printf("Enter your roll number: ");
     scanf("%s",roll);
 
+    //lowers all the character to the lower case by passing the pointer of the array of characters
+    toLower(name);
+    toLower(roll);
+
     char email[50];
-    sprintf(email, "%s_%s@iitp.ac.in", name,roll);//task1   -->need to make sure the name and roll number are lower caps
+    sprintf(email, "%s_%s@iitp.ac.in", name,roll);
 
     struct Credentials credential=fetchCredentials(email);
 
@@ -94,15 +108,23 @@ void registerUser()
         scanf("%s",pass);
 
         struct Student student;
-        strcpy(student.name,name);
-        student.nMessages=0;
-        student.nBooks=0;
-        storeData(student,email);
+        if(ADMIN==0)
+        {
+            student.nBooks=0;
+            storeData(student,email);
+        }
 
+        int encrypted_password[8];
+        EncryptPasssword(pass,encrypted_password);
+        
         strcpy(credential.email, email);
-        strcpy(credential.password, pass);
-
+        for(int i=0;i<8;i++){
+            credential.password[i]=encrypted_password[i];
+        }
+        
         registerCredentials(credential);
+
+        printf("Registered Successfully!\n");        
     }
     else
     {
@@ -110,12 +132,30 @@ void registerUser()
     }
 }
 
+int compare_password(int input_password[],int actual_password[])
+{
+    int equal = 1;
+    for(int i=0;i<8;i++)
+    {
+        if(input_password[i]!=actual_password[i])
+        {
+            equal=0;
+            break;
+        }
+    }
+
+    return equal;
+}
+
 struct Student loginUser()
 {
     struct Student student;
     char email[50],pass[50];
+    int Encrypted_password[8];
     printf("Enter your email: ");
     scanf("%s",email);
+
+    toLower(email);
 
     struct Credentials credential=fetchCredentials(email);
 
@@ -128,15 +168,20 @@ struct Student loginUser()
     printf("Enter password: ");
     scanf("%s",pass);
 
-    if(strcmp(credential.password,pass)==0)
+    EncryptPasssword(pass,Encrypted_password);
+
+    if(compare_password(Encrypted_password,credential.password)==1)
     {
-        if(strcmp(credential.email,"admin_admin@iitp.ac.in")==0){
+        if(strcmp(credential.email,"admin_admin@iitp.ac.in")==0)
+        {
             ADMIN = 1;
         }
         login=1;
         sprintf(Email,credential.email);
-        student=fetchData(credential.email);
-
+        if(ADMIN==0)
+        {
+            student=fetchData(credential.email);
+        }
     }
     else
     {
